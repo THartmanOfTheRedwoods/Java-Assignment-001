40b93f4ea8760b72f6c8f08f1be05384748aef13
AlexeiIachkov <156566521+AlexeiIachkov@users.noreply.github.com>
2024-01-14 22:00:36 -0800

    HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,13 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Alexei Iachkov
+ *
+ * @since Version 1.0
+ * @data  1-14-23
+ *
+ */
+
 public class HelloWorld {
 
     public static void main(String[] args) {
